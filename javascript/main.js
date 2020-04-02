4b3bc07e1a9d25365080df0ab7c3660c3c67e771
Eva Jelinkova <jelin.eva@gmail.com>
2020-04-02 19:35:32 +0100

    changed projets

--- javascript/main.js
+++ javascript/main.js
@@ -3,130 +3,151 @@
 
 
 
-const galleryContainer = document.querySelector('.gallery-container');
-const galleryControlsContainer = document.querySelector('.gallery-controls');
-const galleryControls = ['previous', 'next'];
-const galleryItems = document.querySelectorAll('.gallery-item');
+// const galleryContainer = document.querySelector('.gallery-container');
+// const galleryControlsContainer = document.querySelector('.gallery-controls');
+// const galleryControls = ['previous', 'next'];
+// const galleryItems = document.querySelectorAll('.gallery-item');
 
-class Carousel {
-  constructor(container, items, controls) {
-    this.carouselContainer = container;
-    this.carouselControls = controls;
-    this.carouselArray = [...items];
-  }
+// class Carousel {
+//   constructor(container, items, controls) {
+//     this.carouselContainer = container;
+//     this.carouselControls = controls;
+//     this.carouselArray = [...items];
+//   }
 
-  // Assign initial css classes for gallery and nav items
-  setInitialState() {
-    this.carouselArray[0].classList.add('gallery-item-first');
-    this.carouselArray[1].classList.add('gallery-item-previous');
-    this.carouselArray[2].classList.add('gallery-item-selected');
-    this.carouselArray[3].classList.add('gallery-item-next');
-    this.carouselArray[4].classList.add('gallery-item-last');
+//   // Assign initial css classes for gallery and nav items
+//   setInitialState() {
+//     this.carouselArray[0].classList.add('gallery-item-first');
+//     this.carouselArray[1].classList.add('gallery-item-previous');
+//     this.carouselArray[2].classList.add('gallery-item-selected');
+//     this.carouselArray[3].classList.add('gallery-item-next');
+//     this.carouselArray[4].classList.add('gallery-item-last');
 
-    document.querySelector('.gallery-nav').childNodes[0].className = 'gallery-nav-item gallery-item-first';
-    document.querySelector('.gallery-nav').childNodes[1].className = 'gallery-nav-item gallery-item-previous';
-    document.querySelector('.gallery-nav').childNodes[2].className = 'gallery-nav-item gallery-item-selected';
-    document.querySelector('.gallery-nav').childNodes[3].className = 'gallery-nav-item gallery-item-next';
-    document.querySelector('.gallery-nav').childNodes[4].className = 'gallery-nav-item gallery-item-last';
-  }
+//     document.querySelector('.gallery-nav').childNodes[0].className = 'gallery-nav-item gallery-item-first';
+//     document.querySelector('.gallery-nav').childNodes[1].className = 'gallery-nav-item gallery-item-previous';
+//     document.querySelector('.gallery-nav').childNodes[2].className = 'gallery-nav-item gallery-item-selected';
+//     document.querySelector('.gallery-nav').childNodes[3].className = 'gallery-nav-item gallery-item-next';
+//     document.querySelector('.gallery-nav').childNodes[4].className = 'gallery-nav-item gallery-item-last';
+//   }
 
-  // Update the order state of the carousel with css classes
-  setCurrentState(target, selected, previous, next, first, last) {
+//   // Update the order state of the carousel with css classes
+//   setCurrentState(target, selected, previous, next, first, last) {
 
-    selected.forEach(el => {
-      el.classList.remove('gallery-item-selected');
+//     selected.forEach(el => {
+//       el.classList.remove('gallery-item-selected');
 
-      if (target.className == 'gallery-controls-previous') {
-        el.classList.add('gallery-item-next');
-      } else {
-        el.classList.add('gallery-item-previous');
-      }
-    });
+//       if (target.className == 'gallery-controls-previous') {
+//         el.classList.add('gallery-item-next');
+//       } else {
+//         el.classList.add('gallery-item-previous');
+//       }
+//     });
 
-    previous.forEach(el => {
-      el.classList.remove('gallery-item-previous');
+//     previous.forEach(el => {
+//       el.classList.remove('gallery-item-previous');
 
-      if (target.className == 'gallery-controls-previous') {
-        el.classList.add('gallery-item-selected');
-      } else {
-        el.classList.add('gallery-item-first');
-      }
-    });
+//       if (target.className == 'gallery-controls-previous') {
+//         el.classList.add('gallery-item-selected');
+//       } else {
+//         el.classList.add('gallery-item-first');
+//       }
+//     });
 
-    next.forEach(el => {
-      el.classList.remove('gallery-item-next');
+//     next.forEach(el => {
+//       el.classList.remove('gallery-item-next');
 
-      if (target.className == 'gallery-controls-previous') {
-        el.classList.add('gallery-item-last');
-      } else {
-        el.classList.add('gallery-item-selected');
-      }
-    });
+//       if (target.className == 'gallery-controls-previous') {
+//         el.classList.add('gallery-item-last');
+//       } else {
+//         el.classList.add('gallery-item-selected');
+//       }
+//     });
 
-    first.forEach(el => {
-      el.classList.remove('gallery-item-first');
+//     first.forEach(el => {
+//       el.classList.remove('gallery-item-first');
 
-      if (target.className == 'gallery-controls-previous') {
-        el.classList.add('gallery-item-previous');
-      } else {
-        el.classList.add('gallery-item-last');
-      }
-    });
+//       if (target.className == 'gallery-controls-previous') {
+//         el.classList.add('gallery-item-previous');
+//       } else {
+//         el.classList.add('gallery-item-last');
+//       }
+//     });
 
-    last.forEach(el => {
-      el.classList.remove('gallery-item-last');
+//     last.forEach(el => {
+//       el.classList.remove('gallery-item-last');
 
-      if (target.className == 'gallery-controls-previous') {
-        el.classList.add('gallery-item-first');
-      } else {
-        el.classList.add('gallery-item-next');
-      }
-    });
-  }
+//       if (target.className == 'gallery-controls-previous') {
+//         el.classList.add('gallery-item-first');
+//       } else {
+//         el.classList.add('gallery-item-next');
+//       }
+//     });
+//   }
 
-  // Construct the carousel navigation
-  setNav() {
-    galleryContainer.appendChild(document.createElement('ul')).className = 'gallery-nav';
+//   // Construct the carousel navigation
+//   setNav() {
+//     galleryContainer.appendChild(document.createElement('ul')).className = 'gallery-nav';
 
-    this.carouselArray.forEach(item => {
-      const nav = galleryContainer.lastElementChild;
-      nav.appendChild(document.createElement('li'));
-    }); 
-  }
+//     this.carouselArray.forEach(item => {
+//       const nav = galleryContainer.lastElementChild;
+//       nav.appendChild(document.createElement('li'));
+//     }); 
+//   }
 
-  // Construct the carousel controls
-  setControls() {
-    this.carouselControls.forEach(control => {
-      galleryControlsContainer.appendChild(document.createElement('button')).className = `gallery-controls-${control}`;
-    }); 
+//   // Construct the carousel controls
+//   setControls() {
+//     this.carouselControls.forEach(control => {
+//       galleryControlsContainer.appendChild(document.createElement('button')).className = `gallery-controls-${control}`;
+//     }); 
 
-    !!galleryControlsContainer.childNodes[0] ? galleryControlsContainer.childNodes[0].innerHTML = this.carouselControls[0] : null;
-    !!galleryControlsContainer.childNodes[1] ? galleryControlsContainer.childNodes[1].innerHTML = this.carouselControls[1] : null;
-  }
+//     !!galleryControlsContainer.childNodes[0] ? galleryControlsContainer.childNodes[0].innerHTML = this.carouselControls[0] : null;
+//     !!galleryControlsContainer.childNodes[1] ? galleryControlsContainer.childNodes[1].innerHTML = this.carouselControls[1] : null;
+//   }
  
-  // Add a click event listener to trigger setCurrentState method to rearrange carousel
-  useControls() {
-    const triggers = [...galleryControlsContainer.childNodes];
+//   // Add a click event listener to trigger setCurrentState method to rearrange carousel
+//   useControls() {
+//     const triggers = [...galleryControlsContainer.childNodes];
 
-    triggers.forEach(control => {
-      control.addEventListener('click', () => {
-        const target = control;
-        const selectedItem = document.querySelectorAll('.gallery-item-selected');
-        const previousSelectedItem = document.querySelectorAll('.gallery-item-previous');
-        const nextSelectedItem = document.querySelectorAll('.gallery-item-next');
-        const firstCarouselItem = document.querySelectorAll('.gallery-item-first');
-        const lastCarouselItem = document.querySelectorAll('.gallery-item-last');
+//     triggers.forEach(control => {
+//       control.addEventListener('click', () => {
+//         const target = control;
+//         const selectedItem = document.querySelectorAll('.gallery-item-selected');
+//         const previousSelectedItem = document.querySelectorAll('.gallery-item-previous');
+//         const nextSelectedItem = document.querySelectorAll('.gallery-item-next');
+//         const firstCarouselItem = document.querySelectorAll('.gallery-item-first');
+//         const lastCarouselItem = document.querySelectorAll('.gallery-item-last');
 
-        this.setCurrentState(target, selectedItem, previousSelectedItem, nextSelectedItem, firstCarouselItem, lastCarouselItem);
-      });
-    });
+//         this.setCurrentState(target, selectedItem, previousSelectedItem, nextSelectedItem, firstCarouselItem, lastCarouselItem);
+//       });
+//     });
+//   }
+// }
+
+// const exampleCarousel = new Carousel(galleryContainer, galleryItems, galleryControls);
+
+// exampleCarousel.setControls();
+// exampleCarousel.setNav();
+// exampleCarousel.setInitialState();
+// exampleCarousel.useControls();
+
+
+
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
   }
 }
 
-const exampleCarousel = new Carousel(galleryContainer, galleryItems, galleryControls);
-
-exampleCarousel.setControls();
-exampleCarousel.setNav();
-exampleCarousel.setInitialState();
-exampleCarousel.useControls();
-
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
